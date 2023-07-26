604bff58b737ecf630ab3316e773144071b5f964
YulyaVlasenko <correspondenceoff@gmail.com>
2023-07-26 06:20:09 +0300

    first

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,84 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from "react"
+import ContactForm from "./ContactForm/ContactForm"
+import { Section } from "./Section/Section"
+import ContactList from "./ContactList/ContactList"
+import Filter from "./Filter/Filter"
+import toast, { Toaster } from 'react-hot-toast'
+
+
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  }
+
+  componentDidMount = () => {
+    const savedList = localStorage.getItem('contacts')
+    if (savedList)
+      this.setState({
+        contacts: JSON.parse(savedList)
+      })
+  }
+
+
+  componentDidUpdate = (prevProps, prevState) => {
+    if (prevState.contacts !== this.state.contacts)
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+    
+    if (prevState.contacts.length < this.state.contacts.length)
+      toast.success('Create contact successfully!')
+        
+    if (prevState.contacts.length > this.state.contacts.length)
+      toast.error('Delete contact successfully!')
+  }
+  
+
+  createContact = (contact) => {
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, contact]
+    }));
+  }
+
+  getListOfContacts = () => {
+    const { contacts, filter } = this.state;
+    const filterValue = filter.toLowerCase()
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(filterValue)
+    )
+  }
+
+  handleChangeFilter = ({ target: { value } }) => {
+    this.setState({
+      filter: value,
+    })
+  }
+
+
+  deleteContact = (contactForDelete) => {
+    this.setState(prevState =>
+      ({ contacts: prevState.contacts.filter(contact => contact.id !== contactForDelete) })
+    )
+  }
+
+  render() {
+    const { filter, contacts } = this.state
+   
+    return (
+      <>
+        <Section title='Phonebook'>
+          <ContactForm createContact={this.createContact} contacts={contacts} />
+        </Section>
+        
+        <Section title='Contacts'>
+          <Filter title='Find contacts by name' handleChangeFilter={this.handleChangeFilter} value={filter} />
+          <ContactList contacts={this.getListOfContacts()} deleteContact={this.deleteContact} />
+        </Section>
+        <Toaster />
+      </>
+    )
+    
+  }
+
+}
+export default App
